ee31dac2b8262d8a5e51fae2660bf09dd264eae2
rranjan-us <rr.ranjan@gmail.com>
2018-12-29 21:12:31 -0500

    trying to fix sub module

---
+++ worker/index.js
@@ -0,0 +1,22 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  console.log("Index being computed")
+  if (index < 2) return 1;
+    return fib(index-1)+fib(index-2);
+}
+
+sub.on('message',(channel, message) => {
+  console.log("message received");
+  redisClient.hset('values',message, fib(parseInt(message)));
+} );
+
+sub.subscribe('insert');
